55156ad725047827b0a29939844b03cb20ecb2a4
Phill Farrugia <me@phillfarrugia.com>
2015-02-25 21:15:23 +1100

    Configuring local strategy and sessions

--- config/express.js
+++ config/express.js
@@ -8,6 +8,28 @@ var bodyParser = require('body-parser');
 var compress = require('compression');
 var methodOverride = require('method-override');
 
+
+/*
+ * Passportjs Configuration
+ */
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }
+));
+
 module.exports = function(app, config) {
   app.set('views', config.root + '/app/views');
   app.set('view engine', 'jade');
@@ -22,6 +44,11 @@ module.exports = function(app, config) {
   app.use(compress());
   app.use(express.static(config.root + '/public'));
   app.use(methodOverride());
+
+  // enabling passport sessions
+  app.use(express.session({ secret: 'qs2ifATZAxRXFmdupXAE' }));
+  app.use(passport.initialize());
+  app.use(passport.session());
 
   var controllers = glob.sync(config.root + '/app/controllers/*.js');
   controllers.forEach(function (controller) {
