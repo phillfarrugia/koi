99ce00aaaad2972af1b8479ba21a91d0abe37980
Phill Farrugia <me@phillfarrugia.com>
2015-03-25 23:37:18 +1100

    Run a mongodb instance in travis when deployed and mock mongoose before tests

--- test/index.spec.js
+++ test/index.spec.js
@@ -2,9 +2,16 @@ var express = require('express'),
 config = require('../config/config'),
 expect = require('chai').expect,
 app = require('../app.js'),
-request = require('supertest')(app);
+request = require('supertest')(app),
+mongoose = require('mongoose'),
+mockgoose = require('mockgoose');
 
 describe('index', function() {
+
+	before(function(done) {
+		mockgoose(mongoose);
+		done();
+	});
 
 	it('should load the page', function(done) {
 		request
